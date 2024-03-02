8115755c7c6fc631621c88a1a5ef52d1b3d02a9e
htoohtetwin4 <htoohtetwin.hhw4@gmail.com>
2024-03-02 22:39:36 +0630

    hope's dashboard setup with jetstream

---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,29 @@
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+    <!-- <x-application-logo class="block h-12 w-auto" /> -->
+
+    <h1 class="mt-8 text-2xl font-medium text-gray-900">
+        Welcome to H's Dashboard!
+    </h1>
+
+    <p class="mt-6 text-gray-500 leading-relaxed">
+        I will upload photo here.
+    </p>
+</div>
+
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+    <div>
+        <div class="flex items-center">
+
+            <img src="{{URL::asset('/img/p1.jpg')}}" alt="sky pic" height="200" width="200">
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            "The sky is a canvas painted by the hand of nature, where every stroke tells a story of beauty and wonder."
+        </p>
+
+    </div>
+
+
+
+
+</div>
